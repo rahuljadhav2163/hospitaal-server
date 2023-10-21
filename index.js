fe1e7f85bd9db5e6ffef7119282e4eab0b8da628
rahuljadhav2163 <rgj4002@gmail.com>
2023-10-21 20:01:50 +0530

    added delete and put operation

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = 5000;
 const mongoconnect = async ()=>{
     const connextion = await mongoose.connect(process.env.MONGO_URI);
     if(connextion){
-        console.log("MpngoDB connect Succesfullly")
+        console.log("MongoDB connect Succesfullly")
     }
 }
 
@@ -69,6 +69,49 @@ app.get("/specificdoctor" , async(req,res)=>{
   })
 
 })
+
+app.delete('/deletedoc/:id', async (req,res)=>{
+      const {id}= req.params;
+
+      await doctordata.deleteOne({_id : id})
+
+      res.json({
+        success : "true",
+        doctor : {},
+        message : "Succesfully delete Data."
+      })
+
+})
+
+app.put('/updatedoc/:id', async (req,res)=>{
+    const {id} = req.params;
+    const {name,age,number,degree} = req.body;
+   
+    await doctordata.updateOne({
+        _id : id
+    },
+    {
+        $set : {
+            name,age,number,degree
+        }
+    }
+    )
+    if (!name || !age || !number || !degree) {
+        return res.json({
+          success: false,
+          message: "All fields are required",
+        });
+      }
+
+       const updatedDoc = await doctordata.findOne({_id : id})
+       res.json({
+        success : "true",
+        doctor : updatedDoc,
+        message : "Succesfully Fetch Data."
+      })
+
+})
+
 app.listen(PORT , ()=>{
     console.log(`Server is running on port ${PORT}`);
 })
